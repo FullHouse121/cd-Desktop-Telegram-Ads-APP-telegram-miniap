bdd44ce933475d3440f0e356f59be2c57bc9dad4
Leonardo de Lucas Farias Rocha <leonardodelucasfariasrocha@Leonardos-MacBook-Air.local>
2025-08-01 12:39:11 +0400

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyBVRuKaod50BvdaKP2tr516NBD9-p6JtXc",
